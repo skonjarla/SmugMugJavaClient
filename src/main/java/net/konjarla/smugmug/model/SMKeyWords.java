a359aee477e503c5f297d44d5bb5df90d58cff71
Srikanth Konjarla <srikanth.konjarla@gmail.com>
2025-01-12 21:41:21 +0530

    updated models with ToString

--- src/main/java/net/konjarla/smugmug/model/SMKeyWords.java
+++ src/main/java/net/konjarla/smugmug/model/SMKeyWords.java
@@ -5,12 +5,14 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
 import lombok.extern.jackson.Jacksonized;
 
 import java.util.List;
 
 @Data
 @Builder
+@ToString
 @Jacksonized
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
--- src/main/java/net/konjarla/smugmug/model/SMUri.java
+++ src/main/java/net/konjarla/smugmug/model/SMUri.java
@@ -5,10 +5,12 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 import lombok.extern.jackson.Jacksonized;
 
 @Getter
 @Builder
+@ToString
 @Jacksonized
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
